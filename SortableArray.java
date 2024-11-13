115f704bf8c549e4f4e3fe6aafcd325b5daed744
Saahib <saahib.dhanda13@gmail.com>
2024-11-13 15:22:51 -0800

    Add and test integer Sorting Feature

---
+++ SortableArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortableArray {
+    public static void sortIntegers(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 1};
+        sortIntegers(numbers);
+        System.out.println("Sorted integers: " + Arrays.toString(numbers));
+    }
+}
