38a5288b99cbd2194da24eb15f2c7da8534793d7
Saahib <saahib.dhanda13@gmail.com>
2024-11-16 12:00:43 -0800

    Added sortString function

--- SortableArray/SortableArray.java
+++ SortableArray/SortableArray.java
@@ -6,9 +6,23 @@ public class SortableArray {
         Arrays.sort(array);
     }
 
+    public static void sortStrings(String[] array) {
+        Arrays.sort(array);
+    }
+
     public static void main(String[] args) {
         int[] numbers = {5, 3, 8, 1};
         sortIntegers(numbers);
         System.out.println("Sorted integers: " + Arrays.toString(numbers));
+        
+        System.out.println("\n");
+
+        String [] words = {"Apple", "Dog", "Cat", "Pear"};
+        sortStrings(words);
+        System.out.println("Sorted Words : " + Arrays.toString(words));
+
+
+
+
     }
 }
